3edbd5a0f9a8f2b5d08d4f81e3c2b289bded88c9
marianaer <mescobar.rd@gmail.com>
2022-05-12 11:09:31 +0200

    Trinotate pipeline update

--- transcriptome_annotation.sh
+++ transcriptome_annotation.sh
@@ -6,31 +6,18 @@
 
 ### Identify candidate coding regions from Trinity outputs with TransDecoder (ie. looking for ORFs) ###
 
-PATH_TO_DB=/home/escobar/Desktop/databases/trinotate_db
-PATH_TO_TRANSDECODER=/home/escobar/bin/TransDecoder
 Trinity_fasta=$1 #fasta file with transcripts
+Trinity_gene_map=$2 #Gene_trans_map output by trinity
+PATH_TO_DB=/home/escobar/Desktop/databases/trinotate_db
+TRINOTATE_PATH=/home/escobar/bin/Trinotate
+RNAMMER_PATH=/home/escobar/bin/rnammer-1.2
 
 # 1. Extract long ORFs (at least 100 amino acids long)
 #${PATH_TO_TRANSDECODER}/TransDecoder.LongOrfs -t $Trinity_fasta
 
-
-# 2. Identidy ORFs with homology to known proteins via blast or pfam searches
-# Searches candidate peptides from transDecoder for homology using blastp anf pfam
-
-#  BlastP search
-
-#blastp -query ${Trinity_fasta}.transdecoder_dir/longest_orfs.pep -db ${PATH_TO_DB}/uniprot_sprot.pep -max_target_seqs 1 -outfmt 6 -evalue 1e-5 -num_threads 24 > blastp.outfmt6
-
-# Pfam search for protein domains
-#hmmsearch --cpu 8 --domtblout pfam.domtblout ${PATH_TO_DB}/Pfam-A.hmm transdecoder_dir/longest_orfs.pep
-
-
-# Integrating Blast and Pfam searches into coding region selection
-# Make use of blastp and hmmer searches  and report them as likely coding regions.
-#TransDecoder.Predict -t $Trinity_fasta --retain_pfam_hits pfam.domtblout --retain_blastp_hits blastp.outfmt6
-
 #TransDecoder.Predict -t ${Trinity_fasta}
 
+# 2.
 ### Capturing Blast homologies with Trinotate ###
 
 # search Trinity transcripts
@@ -42,6 +29,30 @@ blastp -query ${Trinity_fasta}.transdecoder_dir/longest_orfs.pep  -db ${PATH_TO_
 # Running HMMER to identify protein domains
 hmmsearch --cpu 40 --domtblout TrinotatePFAM.out  ${PATH_TO_DB}/Pfam-A.hmm ${Trinity_fasta}.transdecoder_dir/longest_orfs.pep > pfam.log
 
+## SignalP to predict signal peptides
+signalp6 -f short -n signalp.out ${Trinity_fasta}.transdecoder.pep
+
+#DeepTMHMM (tmHMM) to predict transmembrane regions
+# conda activate tmhmm
+biolib run DTU/DeepTMHMM --fasta ${Trinity_fasta}.transdecoder.pep
+#tmhmm --short < transdecoder.pep > tmhmm.out
+#conda deactivate
+
+#RNAMMER to identify rRNA transcripts
+
+# First, concatenate all transcripts together into a super-scaffold. Then run rnammer to identify rRNA homologies and transform rRNA feature cordinates in the big scaffold back to the transcriptome reference coordinates.
+${TRINOTATE_PATH}/util/rnammer_support/RnammerTranscriptome.pl --transcriptome ${Trinity_fasta} --path_to_rnammer ${RNAMMER_PATH}/rnammer
 
 
-#Every evening from 9:30 pm on, the  Cinema de la Plage provides open-air screenings for all to enjoy
+# Load resulting gff file into trinotate in SQLite databases
+
+# Import transcriptome and protein data
+init --gene_trans_map ${Trinity_gene_map} --transcript_fasta ${Trinity_fasta} --transdecoder_pep ${Trinity_fasta}.transdecoder.pep
+
+# TransDecoder protein search results
+#LOAD_swissprot_blastp blastp.outfmt6
+#LOAD_pfam TrinotatePFAM.out
+#LOAD_tmhmm <file>
+#LOAD_signalp <file>
+
+#${TRINOTATE_PATH}/Trinotate

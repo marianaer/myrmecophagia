894e54c2b7fbc8b91408e078c2cad6b4af880fd0
marianaer <mescobar.rd@gmail.com>
2022-05-18 15:26:42 +0200

    Kallisto

--- kallisto_pipeline.sh
+++ kallisto_pipeline.sh
@@ -9,11 +9,11 @@ TRINITY_PATH=/home/escobar/bin/trinityrnaseq-v2.14.0
 
 # Build kallisto index files from Trinity contigs
 echo 'Building kallisto index files from Trinity contigs...'
-#kallisto index --make-unique -i ${Trinity_fasta}_kallisto.index ${Trinity_fasta}
+kallisto index --make-unique -i ${Trinity_fasta}_kallisto.index ${Trinity_fasta}
 
 # Quantifying expression using paired reads on the kallisto index files
 echo 'Quantifying expression using paired reads on the kallisto index files...'
-#kallisto quant -i ${Trinity_fasta}_kallisto.index -o ${Trinity_fasta}_Trinity_kallisto -b 100 ${fastq1} ${fastq2} -t 40
+kallisto quant -i ${Trinity_fasta}_kallisto.index -o ${Trinity_fasta}_Trinity_kallisto -b 100 ${fastq1} ${fastq2} -t 40
 
 
 ####### Trinity Transcript Quantification #######
@@ -21,6 +21,7 @@ echo 'Quantifying expression using paired reads on the kallisto index files...'
 
 ## Just prepare the reference for alignment and abundance estimation
 echo 'Preparing reference for alignment...'
+
 ${TRINITY_PATH}/util/align_and_estimate_abundance.pl --transcripts ${Trinity_fasta} --est_method kallisto --trinity_mode --prep_reference --output_dir ${Trinity_fasta}_Trinity_kallisto
 
 ## Run the alignment and abundance estimation (assumes reference has already been prepped, errors-out if prepped reference not located.)
@@ -29,7 +30,8 @@ ${TRINITY_PATH}/util/align_and_estimate_abundance.pl --transcripts ${Trinity_fas
 
 ## Use the --gene_trans_map or --trinity_mode parameters in order to get a gene counts matrix in addition to the isoform counts matrix
 echo 'Getting counts matrix...'
-${TRINITY_PATH}/util/abundance_estimates_to_matrix.pl --est_method kallisto  --name_sample_by_base_dir  ${Trinity_fasta}_kallisto/abundance.tsv --gene_trans_map ${Trinity_fasta}.gene.trans.map --out_prefix ${Trinity_fasta}_kallisto
+echo ${Trinity_fasta}_Trinity_kallisto/abundance.tsv
+${TRINITY_PATH}/util/abundance_estimates_to_matrix.pl --est_method kallisto --name_sample_by_basedir ${Trinity_fasta}_Trinity_kallisto/abundance.tsv --gene_trans_map ${Trinity_fasta}.gene_trans_map
 
 
 #/home/escobar/bin/trinityrnaseq-v2.14.0/util/abundance_estimates_to_matrix.pl --est_method kallisto --name_sample_by_basedir kallisto_output/abundance.tsv  --gene_trans_map Choloepus_didactylus_trinity_output.Trinity.fasta.longest.fasta.gene_trans_map

f7d239fe33d9867a5e675bd75d5522ad69eb10bc
marianaer <mescobar.rd@gmail.com>
2022-05-18 14:34:01 +0200

    Kallisto

--- kallisto_pipeline.sh
+++ kallisto_pipeline.sh
@@ -2,13 +2,13 @@
 Trinity_fasta=$1 # fasta file with transcripts
 fastq1=$2 # Right pair of paired end reads
 fastq2=$3 #L eft pair of paired end reads
-output_dir=$4 # Path for outputs
+
 TRINITY_PATH=/home/escobar/bin/trinityrnaseq-v2.14.0
 
 
 
 # Build kallisto index files from Trinity contigs
-kallisto index --make-unique -i ${Trinity_fasta}_kallisto.index ${Trinity_fasta}; done
+kallisto index --make-unique -i ${Trinity_fasta}_kallisto.index ${Trinity_fasta}
 
 # Quantifying expression using paired reads on the kallisto index files
 kallisto quant -i ${Trinity_fasta}_kallisto.index -o ${Trinity_fasta}_Trinity_kallisto -b 100 ${fastq1} ${fastq2} -t 40
@@ -18,10 +18,14 @@ kallisto quant -i ${Trinity_fasta}_kallisto.index -o ${Trinity_fasta}_Trinity_ka
 # Trinity script for abundance Quantification
 
 ## Just prepare the reference for alignment and abundance estimation
-${TRINITY_PATH}/util/align_and_estimate_abundance.pl --transcripts ${Trinity_fasta} --est_method kallisto --trinity_mode --prep_reference --output_dir ${output_dir}
+${TRINITY_PATH}/util/align_and_estimate_abundance.pl --transcripts ${Trinity_fasta} --est_method kallisto --trinity_mode --prep_reference --output_dir ${Trinity_fasta}_Trinity_kallisto
 
 ## Run the alignment and abundance estimation (assumes reference has already been prepped, errors-out if prepped reference not located.)
-${TRINITY_PATH}/util/align_and_estimate_abundance.pl --transcripts ${Trinity_fasta} --seqType fq --left  ${fastq1} --right ${fastq2} --est_method kallisto --trinity_mode --output_dir ${output_dir}
+${TRINITY_PATH}/util/align_and_estimate_abundance.pl --transcripts ${Trinity_fasta} --seqType fq --left  ${fastq1} --right ${fastq2} --est_method kallisto --trinity_mode --output_dir ${Trinity_fasta}_Trinity_kallisto
 
 ## Use the --gene_trans_map or --trinity_mode parameters in order to get a gene counts matrix in addition to the isoform counts matrix
-${TRINITY_PATH}/util/align_and_estimate_abundance.pl --est_method kallisto --gene_trans_map ${Trinity_fasta}.gene.trans.map --out_prefix ${Trinity_fasta}_kallisto --name_sample_by_base_dir  ${Trinity_fasta}_kallisto/abundance.tsv
+${TRINITY_PATH}/util/abundance_estimates_to_matrix.pl --est_method kallisto  --name_sample_by_base_dir  ${Trinity_fasta}_kallisto/abundance.tsv --gene_trans_map ${Trinity_fasta}.gene.trans.map --out_prefix ${Trinity_fasta}_kallisto
+
+
+#/home/escobar/bin/trinityrnaseq-v2.14.0/util/abundance_estimates_to_matrix.pl --est_method kallisto --name_sample_by_basedir kallisto_output/abundance.tsv  --gene_trans_map Choloepus_didactylus_trinity_output.Trinity.fasta.longest.fasta.gene_trans_map
+#http://sepsis-omics.github.io/tutorials/modules/kallisto/

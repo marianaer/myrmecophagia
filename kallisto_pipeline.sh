611d42fafee46b4e06f62f79999a67657e29ae0d
marianaer <mescobar.rd@gmail.com>
2022-05-18 15:01:30 +0200

    kallisto

--- kallisto_pipeline.sh
+++ kallisto_pipeline.sh
@@ -8,22 +8,27 @@ TRINITY_PATH=/home/escobar/bin/trinityrnaseq-v2.14.0
 
 
 # Build kallisto index files from Trinity contigs
-kallisto index --make-unique -i ${Trinity_fasta}_kallisto.index ${Trinity_fasta}
+echo 'Building kallisto index files from Trinity contigs...'
+#kallisto index --make-unique -i ${Trinity_fasta}_kallisto.index ${Trinity_fasta}
 
 # Quantifying expression using paired reads on the kallisto index files
-kallisto quant -i ${Trinity_fasta}_kallisto.index -o ${Trinity_fasta}_Trinity_kallisto -b 100 ${fastq1} ${fastq2} -t 40
+echo 'Quantifying expression using paired reads on the kallisto index files...'
+#kallisto quant -i ${Trinity_fasta}_kallisto.index -o ${Trinity_fasta}_Trinity_kallisto -b 100 ${fastq1} ${fastq2} -t 40
 
 
 ####### Trinity Transcript Quantification #######
 # Trinity script for abundance Quantification
 
 ## Just prepare the reference for alignment and abundance estimation
+echo 'Preparing reference for alignment...'
 ${TRINITY_PATH}/util/align_and_estimate_abundance.pl --transcripts ${Trinity_fasta} --est_method kallisto --trinity_mode --prep_reference --output_dir ${Trinity_fasta}_Trinity_kallisto
 
 ## Run the alignment and abundance estimation (assumes reference has already been prepped, errors-out if prepped reference not located.)
+echo 'Aligning and estimating abundance...'
 ${TRINITY_PATH}/util/align_and_estimate_abundance.pl --transcripts ${Trinity_fasta} --seqType fq --left  ${fastq1} --right ${fastq2} --est_method kallisto --trinity_mode --output_dir ${Trinity_fasta}_Trinity_kallisto
 
 ## Use the --gene_trans_map or --trinity_mode parameters in order to get a gene counts matrix in addition to the isoform counts matrix
+echo 'Getting counts matrix...'
 ${TRINITY_PATH}/util/abundance_estimates_to_matrix.pl --est_method kallisto  --name_sample_by_base_dir  ${Trinity_fasta}_kallisto/abundance.tsv --gene_trans_map ${Trinity_fasta}.gene.trans.map --out_prefix ${Trinity_fasta}_kallisto
 
 

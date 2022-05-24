c825821de426ad4d25c58b7b1fd5a291aab869b5
marianaer <mescobar.rd@gmail.com>
2022-05-24 14:57:20 +0200

    Extracting fastas of our annotated mucins

--- annotate_expression_from_blasts.sh
+++ annotate_expression_from_blasts.sh
@@ -4,7 +4,7 @@
 cut -f1 muc5b_blasts.tsv | sort -u|egrep -o TRINITY_.* > muc5b_isoforms_blast.list
 
 #get kallisto lines for our found mucs
-for i in $(cat Hsunycteris_thomasi_muc5b_isoforms.list);do grep -E $i"(\^|\s)" kallisto.isoform.TPM.not_cross_norm_annotated >>  kallisto_annotated_muc5b.results; done
+for i in $(cat *muc5b_isoforms*.list);do grep -E $i"(\^|\s)" kallisto.isoform.TPM.not_cross_norm_annotated >>  kallisto_annotated_muc5b.results; done
 
 # remove these lines from kallito file
 
@@ -15,7 +15,7 @@ mv kallisto.isoform.TPM.not_cross_norm_annotated.tmp kallisto.isoform.TPM.not_cr
 
 #ADD MUC7 suffix to transcript names
 
-awk '{ print $1"_MUC7_BLAST "$2; }' kallisto_annotated_muc7.results >> kallisto.isoform.TPM.not_cross_norm_annotated
+awk '{ print $1"_MUC5B_BLAST "$2; }' kallisto_annotated_muc7.results >> kallisto.isoform.TPM.not_cross_norm_annotated
 
 
 # Extract sequences and store them in a fasta files
